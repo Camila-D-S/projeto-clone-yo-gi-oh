eebb385d4c2cbe3e75ecad85171d354d0840d964
Camila Dantas dos Santos <110262657+Camila-D-S@users.noreply.github.com>
2025-01-28 10:34:36 -0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,22 @@ const cartoes = document.querySelectorAll(".cartao");
 
 let cartaoAtual = 0
 
+
+cartoes.forEach(cartao =>{
+    cartao.addEventListener("click", function(){
+        const cartaVirada = cartao.querySelector(".carta-virada")
+
+        //virar o cartao
+        cartao.classList.toggle("virar") /*toggle seria alternar*/
+       //mostrar o fundo da carta
+        cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+        const descricao = cartao.querySelector(".descricao");
+        descricao.classList.toggle("esconder");
+    });    
+});
+
+
 btnAvancar.addEventListener("click", function(){
     if(cartaoAtual === cartoes.length - 1)//para cartão atual aparecer como 6
     return;
